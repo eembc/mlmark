459c60643101da6ae44dcb8a4b4237009b0a613b
petertorelli <peter@petertorelli.com>
2019-10-15 16:52:49 -0700

    Bumped version to 1.0.1.

--- harness/eelib/constants.py
+++ harness/eelib/constants.py
@@ -10,7 +10,7 @@
 # effective EEMBC Benchmark License Agreement, you must discontinue use.
 # 
 
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 
 # Note: developer errors are CRITICAL, user config errors are ERRORS
 
